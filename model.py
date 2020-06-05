da20d43a9f7a6ef4599ae86088499d2741ce885c
吴成龙 <wuchenglong126@126.com>
2020-06-05 15:17:54 +0800

    修复对 transition_params 更新逻辑

--- model.py
+++ model.py
@@ -14,14 +14,12 @@ class NerModel(tf.keras.Model):
         self.num_hidden = hidden_num
         self.vocab_size = vocab_size
         self.label_size = label_size
-        self.transition_params = None
 
         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_size)
         self.biLSTM = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(hidden_num, return_sequences=True))
         self.dense = tf.keras.layers.Dense(label_size)
 
-        self.transition_params = tf.Variable(tf.random.uniform(shape=(label_size, label_size)),
-                                             trainable=False)
+        self.transition_params = tf.Variable(tf.random.uniform(shape=(label_size, label_size)))
         self.dropout = tf.keras.layers.Dropout(0.5)
 
     # @tf.function
@@ -34,8 +32,10 @@ class NerModel(tf.keras.Model):
 
         if labels is not None:
             label_sequences = tf.convert_to_tensor(labels, dtype=tf.int32)
-            log_likelihood, self.transition_params = tf_ad.text.crf_log_likelihood(logits, label_sequences, text_lens)
-            self.transition_params = tf.Variable(self.transition_params, trainable=False)
+            log_likelihood, self.transition_params = tf_ad.text.crf_log_likelihood(logits,
+                                                                                   label_sequences,
+                                                                                   text_lens,
+                                                                                   transition_params=self.transition_params)
             return logits, text_lens, log_likelihood
         else:
             return logits, text_lens
